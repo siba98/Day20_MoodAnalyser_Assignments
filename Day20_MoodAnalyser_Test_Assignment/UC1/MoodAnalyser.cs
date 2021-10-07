8875ac63678d1c5a20b664d375c2b09acdfd8811
siba98 <gyansiba.patro@gmail.com>
2021-10-07 23:47:04 +0530

    Siba:[Added] Mood Analyser For Testing Mood

---
+++ Day20_MoodAnalyser_Test_Assignment/UC1/MoodAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using Day20_MoodAnalyser_Test_Assignment.CustomException;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MoodAnalyser_Test_Assignment.UC1
+{
+    public class MoodAnalyser
+    {
+        //variable
+        string message;
+
+        //constructor with a string type parameter
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        //method for checking mood
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyser_CustomException(MoodAnalyser_CustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyser_CustomException(MoodAnalyser_CustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+}
