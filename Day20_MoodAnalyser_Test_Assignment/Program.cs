67ba239c487e843f9f8b85315b67f69bb2cae47a
siba98 <gyansiba.patro@gmail.com>
2021-10-12 19:00:07 +0530

    Welcome to Mood Analyser Program

---
+++ Day20_MoodAnalyser_Test_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day20_MoodAnalyser_Test_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************WELCOME TO MOOD ANALYSER PROGRAMM WITH EXCEPTION HANDLING******************");
+        }
+    }
+}
