a1a32d6ae7e2d3e14642ac0965df22e8bef5973b
siba98 <gyansiba.patro@gmail.com>
2021-10-13 21:16:11 +0530

    Siba:[Modified] Method with Parameterized Constructor and Added Invoke Method

--- Day20_MoodAnalyser_Test_Assignment/MoodAnalyserFactory.cs
+++ Day20_MoodAnalyser_Test_Assignment/MoodAnalyserFactory.cs
@@ -62,5 +62,22 @@ namespace Day20_MoodAnalyser_Test_Assignment
 
             }
         }
+
+        //below code is UC6
+        public static string InvokeAnalyserMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("Day20_MoodAnalyser_Test_Assignment.MoodAnalyser");
+                object moodAnalyserObject = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterizedConstructor("Day20_MoodAnalyser_Test_Assignment.MoodAnalyser", "MoodAnalyser", message);
+                MethodInfo analyseMoodInfo = type.GetMethod(methodName);
+                object mood = analyseMoodInfo.Invoke(moodAnalyserObject, null);
+                return mood.ToString();
+            }
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Method is Not Found");
+            }
+        }
     }
 }
